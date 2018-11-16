3d2e8275bd4f9d49478c463a123323be543fffc6
DiomChen <Diomchen@users.noreply.github.com>
2018-11-16 19:38:20 +0800

    完成定容栈，但是无法读取txt文件

--- ch1/1.3/src/FixedCapacityStackOfStringEx/FixedCapacityStackOfString.java
+++ ch1/1.3/src/FixedCapacityStackOfStringEx/FixedCapacityStackOfString.java
@@ -1,10 +1,24 @@
 package FixedCapacityStackOfStringEx;
 
 public class FixedCapacityStackOfString {
+    private String [] a;
+    private int N;
     public FixedCapacityStackOfString(int cap){
-
+        a = new String [cap];
     }
 
+    public boolean isEmpty(){
+        return N == 0;
+    }
 
+    public int size(){
+        return N;
+    }
 
+    public void push(String str){
+        a[N++] = str;
+    }
+    public String pop(){
+        return a[N--];
+    }
 }
--- ch1/1.3/src/FixedCapacityStackOfStringEx/TestFixed.java
+++ ch1/1.3/src/FixedCapacityStackOfStringEx/TestFixed.java
@@ -1,8 +1,25 @@
 package FixedCapacityStackOfStringEx;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class TestFixed {
     public static void main(String[] args) {
-        FixedCapacityStackOfString s = new
+        StdOut.println("Please input the caps : ");
+        FixedCapacityStackOfString s = new FixedCapacityStackOfString(StdIn.readInt());
+        StdOut.println("Please input some string and split by <space>:");
+        while(!StdIn.isEmpty()){
+            String k = StdIn.readString();
+            if(!k.equals("-")){
+                s.push(k);
+            }
+            else if(!s.isEmpty()){
+                StdOut.print(s.pop()+" ");
+            }
+        }
+        StdOut.println("( "+ s.size()+ " left on stack" );
+
+
     }
 
 }
