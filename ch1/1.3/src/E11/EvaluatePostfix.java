9a6c24518449e88cd9ec55477515291360b7733f
Diomchen <Diomchen@users.noreply.github.com>
2018-11-18 15:51:02 +0800

    完成1.3.11后序表达式的计算

---
+++ ch1/1.3/src/E11/EvaluatePostfix.java
@@ -0,0 +1,42 @@
+package E11;
+
+import E4.MyStack;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class EvaluatePostfix {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        MyStack<Double> MsDb = new MyStack<>();
+        char[] chArr = str.toCharArray();
+        for(int i=0 ; i<str.length() ; i++){
+            if(Objects.equals(chArr[i], '+')){
+                double x = MsDb.remove();
+                x = MsDb.remove()+x;
+                MsDb.push(x);
+            }
+            else if(Objects.equals(chArr[i], '-')){
+                double x = MsDb.remove();
+                x = MsDb.remove()-x;
+                MsDb.push(x);
+            }
+            else if(Objects.equals(chArr[i], '*')){
+                double x = MsDb.remove();
+                x = MsDb.remove()*x;
+                MsDb.push(x);
+            }
+            else if(Objects.equals(chArr[i], '/')){
+                double x = MsDb.remove();
+                x = MsDb.remove()/x;
+                MsDb.push(x);
+            }
+            else {
+                MsDb.push(Double.parseDouble(String.valueOf(chArr[i])));
+            }
+        }
+        StdOut.println(MsDb.remove());
+    }
+}
