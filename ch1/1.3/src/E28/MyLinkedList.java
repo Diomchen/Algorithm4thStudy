931c1aa9e923d48dd317143f57941a5320101f1f
Diomchen <Diomchen@users.noreply.github.com>
2018-11-18 20:15:25 +0800

    完成1.3.27——完成max函数及以上的题目

--- ch1/1.3/src/E28/MyLinkedList.java
+++ ch1/1.3/src/E28/MyLinkedList.java
@@ -97,13 +97,12 @@ public class MyLinkedList<E> implements Iterable<E> {
         int n = myLinkedList.N;
         for(int i=0 ; i<N ; i++){
             if(be.e == key){
+                be = be.next;
                 if(i == 0){
-                    be = be.next;
                     E eTemp =  removeFirst();
                     i--;
                 }
                 else if(i<N-1){
-                    be = be.next;
                     getNode(i-1).next = getNode(i+1);
                     N--;
                     i--;
@@ -117,6 +116,23 @@ public class MyLinkedList<E> implements Iterable<E> {
                 be = be.next;
             }
         }
+    }
+
+    //仅适用于键值为整数的情况下
+    public int max(Node f){
+        int max = -99999;
+        if(f == null){
+            return 0;
+        }
+        else {
+            while(f!=null){
+                if((Integer)f.e>max){
+                    max = (Integer)f.e;
+                }
+                f = f.next;
+            }
+        }
+        return max;
     }
 
     public Node getNode(int index){
@@ -149,16 +165,22 @@ public class MyLinkedList<E> implements Iterable<E> {
     }
 
     public static void main(String[] args) {
-        MyLinkedList<String> ml = new MyLinkedList<>();
-        ml.addFirst("1");
-        ml.addFirst("2");
-        ml.addFirst("3");
-        ml.addFirst("4");
-        ml.addFirst("3");
-        ml.remove(ml,"3");
-        StdOut.println(ml.removeFirst());
-        StdOut.println(ml.removeFirst());
-        StdOut.println(ml.removeFirst());
-
+//        MyLinkedList<String> ml = new MyLinkedList<>();
+//        ml.addFirst("3");
+//        ml.addFirst("1");
+//        ml.addFirst("2");
+//        ml.addFirst("4");
+//        ml.addFirst("3");
+//        ml.remove(ml,"3");
+//        StdOut.println(ml.removeFirst());
+//        StdOut.println(ml.removeFirst());
+//        StdOut.println(ml.removeFirst());
+        MyLinkedList<Integer> ml = new MyLinkedList<>();
+        ml.addFirst(1);
+        ml.addFirst(2);
+        ml.addFirst(33);
+        ml.addFirst(4);
+        ml.addFirst(5);
+        StdOut.println(ml.max(ml.first));
     }
 }
