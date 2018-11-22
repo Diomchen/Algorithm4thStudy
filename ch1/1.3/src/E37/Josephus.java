818d28135939c16739edfa170be69376902179c2
Diomchen <Diomchen@users.noreply.github.com>
2018-11-22 21:11:06 +0800

    完成1.3.38——未完成37，开始GeneralizedQueue——[0%]

---
+++ ch1/1.3/src/E37/Josephus.java
@@ -0,0 +1,23 @@
+package E37;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @ClassName: Josephus
+ * @Description: 未完成
+ * @Author: DiomChen
+ * @Date: 2018/11/22 20:28
+ */
+public class Josephus {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input N and M:");
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+
+
+    }
+}
---
+++ ch1/1.3/src/E38/GeneralizedQueue.java
@@ -0,0 +1,10 @@
+package E38;
+
+/**
+ * @ClassName: GeneralizedQueue
+ * @Description: TODO
+ * @Author: DiomChen
+ * @Date: 2018/11/22 21:09
+ */
+public class GeneralizedQueue<E> {
+}
