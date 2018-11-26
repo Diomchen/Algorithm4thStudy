5d2c7af90e0519150c9e418402e1dfdf1f4adbbb
Diomchen <Diomchen@users.noreply.github.com>
2018-11-26 18:38:29 +0800

    完成1.3.40——完成MoveToFront——[100%]

--- ch1/1.3/src/E40/MoveToFront.java
+++ ch1/1.3/src/E40/MoveToFront.java
@@ -2,7 +2,7 @@ package E40;
 
 /**
  * @ClassName: MoveToFront
- * @Description: 前移编码
+ * @Description: 前移编码  程序缺点就是remove函数写的比较啰嗦
  * @Author: DiomChen
  * @Date: 2018/11/23 20:53
  */
@@ -49,7 +49,7 @@ public class MoveToFront {
         N++;
     }
 
-    public Character remove(int i){
+    public Character remove(int i){//1
         int k = 0;
         Node temp = first;
         while(i!=k+1){
@@ -62,15 +62,20 @@ public class MoveToFront {
             first = null;
             return temp.e;
         }
-        else if(N != 1 && i == 1 ){
+        else if(N != 1 && i == N ){
             N--;
-            Node node = getNodeByIndex(1);
+            Node node = getNodeByIndex(N-1);
             node.next = null;
             return temp.e;
         }
-        else{
+        else if(N !=1 && i == 1){
             N--;
-            Node node = getNodeByIndex(k);
+            first = first.next;
+            return temp.e;
+        }
+        else {
+            N--;
+            Node node = getNodeByIndex(k-1);
             node.next = temp.next;
             return temp.e;
         }
@@ -108,6 +113,7 @@ public class MoveToFront {
         char[] charArr = str.toCharArray();
         for(int i=0 ; i<charArr.length ; i++){
             if(contains(charArr[i])){
+                System.out.println(mc);
                 remove(mc);
             }
             add(charArr[i]);
@@ -117,7 +123,7 @@ public class MoveToFront {
     public static void main(String[] args) {
         MoveToFront m2f = new MoveToFront();
 
-        m2f.operatorStr("abdidka");
+        m2f.operatorStr("abdda");
 
     }
 }
